63bfdf21e89d1799ca2030a59c367c229a7dd930
Cancu <52618089+Cancuuu@users.noreply.github.com>
2020-08-07 12:35:10 -0300

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,29 +1,43 @@
 
+
 let count = 0;
 
 const value = document.getElementById('number');
-const minus = document.getElementById('btn minus')
-const reset = document.getElementById('btn reset')
-const plus = document.getElementById('btn plus')
-const colorminus = document.querySelector('#number')
+const minus = document.getElementById('btn minus');
+const reset = document.getElementById('btn reset');
+const plus = document.getElementById('btn plus');
+const colorminus = document.querySelector('#number');
+const random = document.getElementById('random')
 
 
-minus.addEventListener('click', function(){
+function main(){
+
+    random.addEventListener('click', function(){
+    count = 0;
+    Rnumber = Math.floor((Math.random()*200)-100);
+    value.innerHTML = Rnumber;
+});
+    minus.addEventListener('click', function(){
     count--;
     value.innerHTML = count;
-    colorminus.style.color = 'red';
+    
 });
 
-plus.addEventListener('click', function(){
+    plus.addEventListener('click', function(){
     count++;
     value.innerHTML = count;
-    colorminus.style.color = 'green';
+    
 });
 
-reset.addEventListener('click', function(){
+    reset.addEventListener('click', function(){
     count = 0;
-    value.innerHTML = count;
-    colorminus.style.color = 'whitesmoke'
+    value.innerHTML = count;    
+    
 });
+};
+
+
+
+main();
 
 
